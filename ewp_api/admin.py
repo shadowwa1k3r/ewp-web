ed0215a432f6e39165f9453709eafc206941a23b
shadowwa1k3r <ergash1994@gmail.com>
2019-02-28 14:13:46 +0500

    reqs

--- ewp_api/admin.py
+++ ewp_api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Council, EwpUser, Feedback, Aviarace, Apartment, Img
+from .models import Council, EwpUser, Feedback, Aviarace, Apartment, Img, FcmDevices
 
 admin.site.register(Council)
 admin.site.register(EwpUser)
@@ -7,3 +7,4 @@ admin.site.register(Feedback)
 admin.site.register(Aviarace)
 admin.site.register(Apartment)
 admin.site.register(Img)
+admin.site.register(FcmDevices)
